327921cb93beeffdf700693caca84ffe0147d4ae
PicoJr <PicoJr@notanemail.com>
2019-12-22 16:06:04 +0100

    Updates

---
+++ precache-manifest.624ac3fb4d3f2d8492addc0f6c1d83bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d902cdc9bfc7d40d7dfd72c5ced9945",
+    "url": "//picojr.github.io/bobo/index.html"
+  },
+  {
+    "revision": "efa03814fc72c8020a9c",
+    "url": "//picojr.github.io/bobo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4e8edbfbfed0f00d2f26",
+    "url": "//picojr.github.io/bobo/static/js/2.8bc22755.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "//picojr.github.io/bobo/static/js/2.8bc22755.chunk.js.LICENSE"
+  },
+  {
+    "revision": "efa03814fc72c8020a9c",
+    "url": "//picojr.github.io/bobo/static/js/main.c994cdd6.chunk.js"
+  },
+  {
+    "revision": "9802e52ea4ca6b38f83b",
+    "url": "//picojr.github.io/bobo/static/js/runtime-main.571c2ee0.js"
+  }
+]);
